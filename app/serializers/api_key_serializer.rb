8c0f4aa726e82bdd11491aab02315825d88e65d8
ignatius22 <igee54077@gmail.com>
2025-03-18 16:22:24 +0100

    fix: Fix issues arround serializer

--- app/serializers/api_key_serializer.rb
+++ app/serializers/api_key_serializer.rb
@@ -1,6 +1,6 @@
 class ApiKeySerializer
   include JSONAPI::Serializer
 
-  attributes :id, :key, :status, :usage_count, :last_used_at, :created_at, :expires_at, :iso8601
+  attributes :id, :key, :status, :usage_count, :last_used_at, :created_at, :expires_at
   belongs_to :user, serializer: UserSerializer
 end
