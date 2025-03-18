09f73d32c9d1ad69193fe0356f45f786e6319ab1
ignatius22 <igee54077@gmail.com>
2025-03-18 17:07:56 +0100

    fix: Fix issues arroud package install render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,6 +2,7 @@
 set -o errexit
 
 bundle install
+npm install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate # Run migrations during build (free tier limitation)
