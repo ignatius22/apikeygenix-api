afb2eb1143e304a67e56e4c68b3e1cadf178a57e
ignatius22 <igee54077@gmail.com>
2025-03-18 17:12:26 +0100

    fix: Fix issues arroud package install render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -o errexit
 
+if ! command -v node &> /dev/null; then
+  curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
+  apt-get install -y nodejs
+fi
+bun
+
 bundle install
 npm install
 bundle exec rails assets:precompile
